2809c2694066f7880a063786bad249820a854b68
Nicole <nicolefongjw@gmail.com>
2026-06-19 16:52:39 -0700

    layout file

--- components/ClientLayout.js
+++ components/ClientLayout.js
@@ -1,17 +1,14 @@
 "use client";
-import { useState } from "react";
 import CustomCursor from "./CustomCursor";
 import LoadingScreen from "./LoadingScreen";
 import PageTransition from "./PageTransition";
 
 export default function ClientLayout({ children }) {
-  const [ready, setReady] = useState(false);
-
   return (
     <>
       <CustomCursor />
-      <LoadingScreen onDone={() => setReady(true)} />
-      {ready && <PageTransition>{children}</PageTransition>}
+      <LoadingScreen />
+      <PageTransition>{children}</PageTransition>
     </>
   );
 }
